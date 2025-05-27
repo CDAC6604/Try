5919031dadffbf38ff1b24e894284503bac8b0a6
unknown <Manishrajput6604@gmail.com>
2025-05-27 21:02:38 +0530

    commit try

---
+++ frontend/src/Component/Try.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+
+const Try = () => {
+  return (
+    
+    <div><Button variant="text" color="primary">
+      try
+    </Button></div>
+  )
+}
+
+export default Try
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Try from '../Component/Try' 
+
+const Home = () => {
+  return (
+    <div>
+        <Try/>
+    </div>
+  )
+}
+
+export default Home
